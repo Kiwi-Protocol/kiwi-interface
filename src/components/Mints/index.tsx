75e7091af2f60bdce2a33a704df0a49c2fc9ae40
jaymalave <jaym.github@gmail.com>
2023-10-20 11:16:14 +0530

    feat: update route

--- src/components/Mints/index.tsx
+++ src/components/Mints/index.tsx
@@ -2,8 +2,10 @@ import React from "react";
 import styles from "./index.module.css";
 import { Button } from "antd";
 import Link from "next/link";
+import { useNavigationStore } from "@/states/navState.state";
 
 export default function Mints() {
+    const setNavState = useNavigationStore((state: any) => state.setNavState);
     return (
         <div className={styles.mintsContainer}>
             <h1>My Mints</h1>
@@ -20,11 +22,15 @@ export default function Mints() {
                         <div className={styles.mintDetails}>
                             <h3>Mint #{i + 1}</h3>
                             <p>Buy Price: 0.1 ETH</p>
-                            <Link href={"/edit/" + (i + 1)}>
-                                <Button className={styles.updateButton}>
-                                    Update
-                                </Button>
-                            </Link>
+
+                            <Button
+                                className={styles.updateButton}
+                                onClick={() => {
+                                    setNavState("mintAvatar");
+                                }}
+                            >
+                                Update
+                            </Button>
                         </div>
                     </div>
                 ))}
