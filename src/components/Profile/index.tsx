5febaa3ada129f88913a51b32fccfe0cbcc0756f
Neelansh Mathur <53081208+neelansh15@users.noreply.github.com>
2023-10-21 07:00:09 +0530

    ft: profile page achievements ui

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -14,6 +14,8 @@ export default function Profile() {
 
     const { nfts, loading } = useNFTs();
 
+    const [selectedNFT, setSelectedNFT] = useState(nfts[0] as any);
+
     useEffect(() => {
         handleGetAddress();
     }, [walletClient]);
@@ -22,6 +24,10 @@ export default function Profile() {
         handleGetAddress();
     }, []);
 
+    useEffect(() => {
+        if (nfts.length > 0) setSelectedNFT(nfts[0]);
+    }, [nfts]);
+
     const handleGetAddress = async () => {
         if (walletClient) {
             const [address] = await walletClient.getAddresses();
@@ -29,7 +35,9 @@ export default function Profile() {
         }
     };
 
-    const onChangeProfile: MenuProps["onClick"] = async ({ key }) => {};
+    const onChangeProfile: MenuProps["onClick"] = async ({ key }) => {
+        setSelectedNFT(nfts[+key]);
+    };
 
     const items: MenuProps["items"] = nfts.map((item: any, i) => ({
         key: i,
@@ -81,7 +89,8 @@ export default function Profile() {
                                             <>
                                                 <img
                                                     src={
-                                                        nfts[0].metadata[0]
+                                                        selectedNFT &&
+                                                        selectedNFT.metadata[0]
                                                             .image
                                                     }
                                                     alt="mint"
@@ -91,8 +100,11 @@ export default function Profile() {
                                                     }}
                                                 />
                                                 <p>
-                                                    {nfts[0].symbol} #
-                                                    {nfts[0].token_id}
+                                                    {selectedNFT &&
+                                                        selectedNFT.symbol}{" "}
+                                                    #
+                                                    {selectedNFT &&
+                                                        selectedNFT.token_id}
                                                 </p>
                                             </>
                                         )}
@@ -102,7 +114,6 @@ export default function Profile() {
                             </Dropdown>
                         }
                         style={{
-                            marginTop: "20px",
                             display: "flex",
                             justifyContent: "space-around",
                             alignItems: "center",
@@ -111,8 +122,8 @@ export default function Profile() {
                     >
                         <img
                             src={
-                                nfts.length > 0
-                                    ? nfts[0].metadata[0].image
+                                nfts.length > 0 && selectedNFT
+                                    ? selectedNFT.metadata[0].image
                                     : "/mock_pixel.jpeg"
                             }
                             alt="profile"
@@ -122,12 +133,27 @@ export default function Profile() {
                                 textAlign: "center",
                             }}
                         >
-                            {nfts.length > 0
-                                ? nfts[0].metadata[0].name
+                            {nfts.length > 0 && selectedNFT
+                                ? selectedNFT.metadata[0].name
                                 : "Some Avatar ;)"}
                         </h4>
                     </Card>
-                    <div className={styles.acheivementsContainer}></div>
+                    <div className={styles.achievementsContainer}>
+                        {selectedNFT &&
+                            selectedNFT.metadata[0].achievments.map(
+                                (item: any) => (
+                                    <Card
+                                        title={item.name}
+                                        extra={<p>{item.experience} XP</p>}
+                                        style={{
+                                            marginBottom: "10px"
+                                        }}
+                                    >
+                                        {item.description}
+                                    </Card>
+                                )
+                            )}
+                    </div>
                 </div>
             )}
         </div>
