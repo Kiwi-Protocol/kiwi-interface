41889a4d82c89f49da9374aba0f44c2503a213ed
jaymalave <jaym.github@gmail.com>
2023-10-21 06:43:18 +0530

    feat: add profile layout flex

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -62,67 +62,73 @@ export default function Profile() {
             {loading ? (
                 <p>Loading...</p>
             ) : (
-                <Card
-                    title={
-                        <Dropdown
-                            menu={{
-                                onClick: onChangeProfile,
-                                items: items,
-                            }}
-                        >
-                            <a onClick={(e) => e.preventDefault()}>
-                                <Space
-                                    style={{
-                                        cursor: "pointer",
-                                    }}
-                                >
-                                    {nfts.length > 0 && (
-                                        <>
-                                            <img
-                                                src={nfts[0].metadata[0].image}
-                                                alt="mint"
-                                                width={"50px"}
-                                                style={{
-                                                    marginRight: "10px",
-                                                }}
-                                            />
-                                            <p>
-                                                {nfts[0].symbol} #
-                                                {nfts[0].token_id}
-                                            </p>
-                                        </>
-                                    )}
-                                    <CaretDownOutlined />
-                                </Space>
-                            </a>
-                        </Dropdown>
-                    }
-                    style={{
-                        marginTop: "20px",
-                        display: "flex",
-                        justifyContent: "space-around",
-                        alignItems: "center",
-                        flexDirection: "column",
-                    }}
-                >
-                    <img
-                        src={
-                            nfts.length > 0
-                                ? nfts[0].metadata[0].image
-                                : "/mock_pixel.jpeg"
+                <div className={styles.profileFlex}>
+                    <Card
+                        title={
+                            <Dropdown
+                                menu={{
+                                    onClick: onChangeProfile,
+                                    items: items,
+                                }}
+                            >
+                                <a onClick={(e) => e.preventDefault()}>
+                                    <Space
+                                        style={{
+                                            cursor: "pointer",
+                                        }}
+                                    >
+                                        {nfts.length > 0 && (
+                                            <>
+                                                <img
+                                                    src={
+                                                        nfts[0].metadata[0]
+                                                            .image
+                                                    }
+                                                    alt="mint"
+                                                    width={"50px"}
+                                                    style={{
+                                                        marginRight: "10px",
+                                                    }}
+                                                />
+                                                <p>
+                                                    {nfts[0].symbol} #
+                                                    {nfts[0].token_id}
+                                                </p>
+                                            </>
+                                        )}
+                                        <CaretDownOutlined />
+                                    </Space>
+                                </a>
+                            </Dropdown>
                         }
-                        alt="profile"
-                    />
-                    <h4
                         style={{
-                            textAlign: "center",
+                            marginTop: "20px",
+                            display: "flex",
+                            justifyContent: "space-around",
+                            alignItems: "center",
+                            flexDirection: "column",
                         }}
                     >
-                        {nfts.length > 0
-                            ? nfts[0].metadata[0].name
-                            : "Some Avatar ;)"}
-                    </h4>
-                </Card>
+                        <img
+                            src={
+                                nfts.length > 0
+                                    ? nfts[0].metadata[0].image
+                                    : "/mock_pixel.jpeg"
+                            }
+                            alt="profile"
+                        />
+                        <h4
+                            style={{
+                                textAlign: "center",
+                            }}
+                        >
+                            {nfts.length > 0
+                                ? nfts[0].metadata[0].name
+                                : "Some Avatar ;)"}
+                        </h4>
+                    </Card>
+                    <div className={styles.acheivementsContainer}></div>
+                </div>
             )}
         </div>
     );
